ccbf83ffddac6fe0c468344661d20e598ba6a870
Samu Burgueño <samuburugeno@gmail.com>
2019-03-14 12:31:48 -0300

    Updates

--- precache-manifest.7b08ab5e86d85b4da7d01b5e3c217714.js
+++ precache-manifest.7b08ab5e86d85b4da7d01b5e3c217714.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "https://samuburgueno.github.io/turnos-cmd/static/js/runtime~main.c8fe6400.js"
   },
   {
-    "revision": "890e78ee5ea7d1cc3575",
-    "url": "https://samuburgueno.github.io/turnos-cmd/static/js/main.8d99eb5d.chunk.js"
+    "revision": "f352c8e900c7bfadfae3",
+    "url": "https://samuburgueno.github.io/turnos-cmd/static/js/main.3b31565d.chunk.js"
   },
   {
     "revision": "23bf3fc41643f8e6a673",
     "url": "https://samuburgueno.github.io/turnos-cmd/static/js/2.a18ca41f.chunk.js"
   },
   {
-    "revision": "890e78ee5ea7d1cc3575",
+    "revision": "f352c8e900c7bfadfae3",
     "url": "https://samuburgueno.github.io/turnos-cmd/static/css/main.33f720f0.chunk.css"
   },
   {
-    "revision": "76abb36e487238ae50bbc33ce97ec76a",
+    "revision": "9ee4484fe097063027ffe61f0628ad66",
     "url": "https://samuburgueno.github.io/turnos-cmd/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://samuburgueno.github.io/turnos-cmd/precache-manifest.383fb7b05131d8bfa899ff99dee6252b.js"
+  "https://samuburgueno.github.io/turnos-cmd/precache-manifest.7b08ab5e86d85b4da7d01b5e3c217714.js"
 );
 
 workbox.clientsClaim();
