98c4b43815d61cb72f33a742b6f2367cff7b2afe
Samu Burgueño <samuburugeno@gmail.com>
2019-03-14 12:35:20 -0300

    Updates

--- precache-manifest.07033fe28fca7e33530e81d17248dce2.js
+++ precache-manifest.07033fe28fca7e33530e81d17248dce2.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "https://samuburgueno.github.io/turnos-cmd/static/js/runtime~main.c8fe6400.js"
   },
   {
-    "revision": "0d924780d856f0bf67f2",
-    "url": "https://samuburgueno.github.io/turnos-cmd/static/js/main.be9b8720.chunk.js"
+    "revision": "0ade45e1380c5748dfae",
+    "url": "https://samuburgueno.github.io/turnos-cmd/static/js/main.6160cb98.chunk.js"
   },
   {
     "revision": "23bf3fc41643f8e6a673",
     "url": "https://samuburgueno.github.io/turnos-cmd/static/js/2.a18ca41f.chunk.js"
   },
   {
-    "revision": "0d924780d856f0bf67f2",
+    "revision": "0ade45e1380c5748dfae",
     "url": "https://samuburgueno.github.io/turnos-cmd/static/css/main.33f720f0.chunk.css"
   },
   {
-    "revision": "4b0425a94267ae8de35d390780cced16",
+    "revision": "8f823d5c6e2b6187877b61e6fe894faf",
     "url": "https://samuburgueno.github.io/turnos-cmd/index.html"
   }
 ];
