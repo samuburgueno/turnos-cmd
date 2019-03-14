bf8383896543535a7ad2d052b7e6df1ba209def6
Samu Burgueño <samuburugeno@gmail.com>
2019-03-14 10:59:48 -0300

    Updates

---
+++ precache-manifest.a82cc6cde10140ca08775f8833f2b640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e68514531f941ea5b953",
+    "url": "/turnos-cmd/static/js/runtime~main.cadf59fd.js"
+  },
+  {
+    "revision": "9bb80c8e1254582b1acb",
+    "url": "/turnos-cmd/static/js/main.22149a6e.chunk.js"
+  },
+  {
+    "revision": "3897d25981a5c6bd2aea",
+    "url": "/turnos-cmd/static/js/2.4e7ff652.chunk.js"
+  },
+  {
+    "revision": "9bb80c8e1254582b1acb",
+    "url": "/turnos-cmd/static/css/main.33f720f0.chunk.css"
+  },
+  {
+    "revision": "5ee645e9ff1bcd21e86509a00e26ec5c",
+    "url": "/turnos-cmd/index.html"
+  }
+];
